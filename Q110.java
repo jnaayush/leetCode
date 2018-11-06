ef3a8efa93120c8efedfbcca04c18c9b61945d7f
Aayush Jain <aayushjain02@gmail.com>
2018-11-06 12:30:35 -0500

    Create Q110.java

---
+++ Q110.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+
+        return ((Math.abs(depth(root.left) - depth(root.right)) <= 1) && isBalanced(root.left) && isBalanced(root.right));
+        
+    }
+    
+    int depth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return Math.max(depth(root.left) + 1, depth(root.right) + 1);
+    }
+    
+}
