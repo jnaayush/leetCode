3ddacb05e8773e8c3cb4843b3fdcacc3b917637d
Aayush Jain <26409957+jnaayush@users.noreply.github.com>
2019-08-23 14:44:15 -0400

    Create Q461.java

---
+++ Q461.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int dist = 0;
+        for(int i = 0; i < 32; i++){
+            if((x&1) != (y&1)){
+                dist++;
+            }
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return dist;
+    }
+}
