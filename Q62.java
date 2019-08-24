567636b7d2b0ca64913a7d1dfdeddb1fdf700034
Aayush Jain <26409957+jnaayush@users.noreply.github.com>
2019-08-24 14:02:16 -0400

    Update Q62.java

--- Q62.java
+++ Q62.java
@@ -1,21 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int steps[][] = new int[m][n];
-        steps[0][0] = 1;
         getSteps(m,n,steps);
-        for(int l = 0; l < steps[0].length; l++){
-            for(int k = 0; k < steps.length; k++){
-                System.out.print(steps[k][l] + " ");
-            }
-            System.out.println(" ");
-        }
         return steps[m-1][n-1];
     }
     
     public void getSteps(int i, int j, int[][] steps){
         for(int m = 0; m < steps.length; m++){
             for(int n = 0; n < steps[0].length; n++){
-                if(m == 0 && n == 0) continue;
+                if(m == 0 || n == 0){
+                    steps[m][n] = 1;
+                    continue;
+                };
                 int left = 0;
                 int up = 0;
                 if(m >= 1)
@@ -27,3 +23,4 @@ class Solution {
         }
     }
 }
+
